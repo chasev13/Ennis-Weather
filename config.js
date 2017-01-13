e8bbe4ebc89307cfc34fc5d751b639e74a160cb3
Ubuntu <ubuntu@ip-172-31-14-136.us-west-2.compute.internal>
2017-01-13 21:26:54 +0000

    removed config file

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         'iAwq3attG122X7UMX8KluaxmR',
-  consumer_secret:      'Wz1xJw9fbU7zn4F1QjLmiwzsiABCgQFzvS24doN5KT0c3ehKJt',
-  access_token:         '819367949484048384-6ymZVbZcdxo9mijVlEz6jOjW0pjR9ya',
-  access_token_secret:  'rFA32XL5DCAmHoCItGE2AwO0fD4iYWwYOyEgH2XoQ0INN'
-};
